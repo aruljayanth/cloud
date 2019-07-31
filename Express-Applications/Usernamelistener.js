db7ee6350231eaeaf8a9ba3facba7df5ca1c46d6
Arul Jayanth <aruljayanth.m.unive@gmail.com>
2019-07-31 23:30:33 +0530

    database work

---
+++ Express-Applications/Usernamelistener.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors());
+
+const validnumber = RegExp((/^[0-9]+$/));
+const validletter = RegExp((/^[A-Za-z]+$/));
+var con = mysql.createConnection({
+  host: "endpoint", user: "yourusername",
+  password: "yourpassword",
+  port: "3306",
+  database: "innodb"
+});
+
+app.post('/', function(req , res){
+  console.log(req.body.firstname);
+  if(req.body==''){
+    res.send("false");
+  }
+  else{
+    fs.appendFile('mynamefile.txt',JSON.stringify(req.body),function (err){
+      if (err) throw err;
+      console.log('saved!!!');
+    });
+    con.connect(function(err) {
+      if (err) throw err;
+      console.log("Connected!");
+      try{
+        con.query("insert into users values(?,?,?,?)", [req.body.firstname,req.body.lastname,req.body.middlename,req.body.age],function (err, result) {
+          console.log("Database created");
+          console.log(result);
+        });
+      }
+      catch(err){
+        console.log(err);
+      }
+      finally{
+        con.end();
+      }
+      });
+    res.send('true');
+  }
+})
+
+app.post('/letters',function(req , res){
+  res.send(validletter.test(req.body.val)?"true":"false")
+})
+
+app.post('/numbers',function(req , res){
+  res.send(validnumber.test(req.body.val)?"true":"false")
+})
+
+//start your server on port 3001
+app.listen(3001, () => {
+  console.log('Server started');
+});
